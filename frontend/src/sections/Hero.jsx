b80d64f240fce5b6b7040ffc12833001ff025b37
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-14 22:15:44 +0000

    feat: add favicon image to Hero section

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -25,7 +25,11 @@ function Hero() {
 
           <span>I CREATE.</span>
         </h1>
+        <div>
+          <img src="favicon.png" alt="" />
 
+
+        </div>
 
         {/* Bottom Content */}
         <div className="hero-bottom">
