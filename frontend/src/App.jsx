e38f56b89d46ccecbebc9fdb681b29d2ba4556ce
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-16 00:48:32 +0000

    feat: implement theme management with local storage support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,27 @@ import Services from "./sections/Services";
 import Skills from "./sections/Skills";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
+import { useEffect, useState } from "react";
+
+
 
 
 
 function App() {
+
+  const [theme, setTheme] = useState(() => {
+  return localStorage.getItem("theme") || "dark";
+  });
+
+  useEffect(() => {
+  document.documentElement.setAttribute(
+    "data-theme",
+    theme
+  );
+
+  localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <div className="app">
 
